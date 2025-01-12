3c88e022d5d6bea60e89e4685f456fa3f6cbde6a
vinodvr81 <vinod.vukkalam@gmail.com>
2025-01-12 08:21:17 +0530

    initital POST check jsonify

--- First_Route_Hello.py
+++ First_Route_Hello.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
     return " Hello Mr. Vinod !"
+
+@app.route('/json', methods=['GET','POST'])
+def check_json():
+    data = request.get_json()
+    if data['name'] and data['age']:
+        return jsonify({'name':data['name'], 'age':data['age']})
+    else:
+        return {}
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
